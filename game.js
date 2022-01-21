931146647ffc9789233ca85e45f41735eed1e248
Rody <rody.van.dijk@gmail.com>
2022-01-21 14:10:42 +0100

    changes shoot alien1 works

--- game.js
+++ game.js
@@ -8,38 +8,196 @@ function change_direction(event) {
   const Digit6 = 54;
   const Digit7 = 55;
   const Digit8 = 56;
+  const KeyE = 69;
 
   const keyPressed = event.keyCode;
   if (keyPressed === Digit1) {
-    document.getElementById("spaceship").style.gridColumnStart = "1";
-    document.getElementById("spaceship").style.gridColumnEnd = "1";
+    document.getElementById("spaceship").style.gridColumn = "1/1";
+    document.getElementById("rocket").style.gridColumn = "1/1";
   }
   if (keyPressed === Digit2) {
-    document.getElementById("spaceship").style.gridColumnStart = "2";
-    document.getElementById("spaceship").style.gridColumnEnd = "2";
+    document.getElementById("spaceship").style.gridColumn = "2/2";
+    document.getElementById("rocket").style.gridColumn = "2/2";
   }
   if (keyPressed === Digit3) {
-    document.getElementById("spaceship").style.gridColumnStart = "3";
-    document.getElementById("spaceship").style.gridColumnEnd = "3";
+    document.getElementById("spaceship").style.gridColumn = "3/3";
+    document.getElementById("rocket").style.gridColumn = "3/3";
   }
   if (keyPressed === Digit4) {
-    document.getElementById("spaceship").style.gridColumnStart = "4";
-    document.getElementById("spaceship").style.gridColumnEnd = "4";
+    document.getElementById("spaceship").style.gridColumn = "4/4";
+    document.getElementById("rocket").style.gridColumn = "4/4";
   }
   if (keyPressed === Digit5) {
-    document.getElementById("spaceship").style.gridColumnStart = "5";
-    document.getElementById("spaceship").style.gridColumnEnd = "5";
+    document.getElementById("spaceship").style.gridColumn = "5/5";
+    document.getElementById("rocket").style.gridColumn = "5/5";
   }
   if (keyPressed === Digit6) {
-    document.getElementById("spaceship").style.gridColumnStart = "6";
-    document.getElementById("spaceship").style.gridColumnEnd = "6";
+    document.getElementById("spaceship").style.gridColumn = "6/6";
+    document.getElementById("rocket").style.gridColumn = "6/6";
   }
   if (keyPressed === Digit7) {
-    document.getElementById("spaceship").style.gridColumnStart = "7";
-    document.getElementById("spaceship").style.gridColumnEnd = "7";
+    document.getElementById("spaceship").style.gridColumn = "7/7";
+    document.getElementById("rocket").style.gridColumn = "7/7";
   }
   if (keyPressed === Digit8) {
-    document.getElementById("spaceship").style.gridColumnStart = "8";
-    document.getElementById("spaceship").style.gridColumnEnd = "8";
+    document.getElementById("spaceship").style.gridColumn = "8/8";
+    document.getElementById("rocket").style.gridColumn = "8/8";
+  }
+  if (keyPressed === KeyE) {
+    document.getElementById("rocket").style.visibility = "visible";
+    document.getElementById("rocket").style.display = "block";
+    up();
   }
 }
+function up() {
+  var i = 14;
+  var intervalId = setInterval(function () {
+    if (i === 1) {
+      clearInterval(intervalId);
+      if (
+        document.getElementById("rocket").style.gridArea ===
+        document.getElementById("alien1").style.gridArea
+      ) {
+;
+        document.getElementById("rocket").style.visibility = "hidden";
+        document.getElementById("rocket").style.display = "none";
+        document.getElementById("alien1").style.visibility = "hidden";
+        document.getElementById("alien1").style.display = "none";
+      } else if (
+        document.getElementById("rocket").style.gridArea !==
+        document.getElementById("alien1").style.gridArea
+      ) {
+        alert("bad");
+      }
+    }
+    document.getElementById("rocket").style.gridRow = i;
+    i--;
+  }, 250);
+}
+function loop1() {
+  var i = "visible";
+  var intervalId = setInterval(function () {
+    if (
+      document.getElementById("rocket").style.gridArea ===
+        document.getElementById("alien1").style.gridArea &&
+      document.getElementById("rocket").style.gridRow === 2
+    ) {
+      document.getElementById("rocket").style.visibility = "hidden";
+      document.getElementById("rocket").style.display = "none";
+      document.getElementById("alien1").style.visibility = "hidden";
+      document.getElementById("alien1").style.display = "none";
+      alert("good");
+      clearInterval(intervalId);
+    } else if (
+      document.getElementById("rocket").style.gridArea !==
+        document.getElementById("alien1").style.gridArea &&
+      document.getElementById("rocket").style.gridRow === 2
+    ) {
+      document.getElementById("rocket").style.visibility = "hidden";
+      document.getElementById("rocket").style.display = "none";
+      alert("bad");
+      clearInterval(intervalId);
+    }
+  }, 100);
+}
+
+// function loop1() {
+//   do {
+// if (
+//   document.getElementById("rocket").style.gridColumn ===
+//     document.getElementById("alien1").style.gridColumn &&
+//   document.getElementById("alien1").style.gridRow ===
+//     document.getElementById("rocket").style.gridRow
+// ) {
+//   document.getElementById("rocket").style.visibility = "hidden";
+//   document.getElementById("rocket").style.display = "none";
+//   document.getElementById("alien1").style.visibility = "hidden";
+//   document.getElementById("alien1").style.display = "none";
+// } else {
+// }
+//   } while (document.getElementById("rocket").style.visibility !== "hidden");
+// }
+
+// var i = 1;
+
+// do {
+//   document.getElementById("rocket").style.gridRow = i;
+//   i++;
+// } while (i < 16);
+// function meteorspeed2() {
+//   setInterval(function () {
+//     document.getElementById("meteor").style.gridRow = "3";
+//   }, 1000);
+//   clearInterval();
+// }
+// var i = 2;
+
+// do {
+//     alert("Hello");
+//     i++;
+// }
+// while (i < 1);
+
+// function rocketspeed1() {
+//   document.getElementById("rocket").style.gridRow = "14";
+// }
+// function rocketspeed2() {
+//   document.getElementById("rocket").style.gridRow = "13";
+// }
+// function rocketspeed3() {
+//   document.getElementById("rocket").style.gridRow = "12";
+// }
+// function rocketspeed4() {
+//   document.getElementById("rocket").style.gridRow = "11";
+// }
+// function rocketspeed5() {
+//   document.getElementById("rocket").style.gridRow = "10";
+// }
+// function rocketspeed6() {
+//   document.getElementById("rocket").style.gridRow = "9";
+// }
+// function rocketspeed7() {
+//   document.getElementById("rocket").style.gridRow = "8";
+// }
+// function rocketspeed8() {
+//   document.getElementById("rocket").style.gridRow = "7";
+// }
+// function rocketspeed9() {
+//   document.getElementById("rocket").style.gridRow = "6";
+// }
+// function rocketspeed10() {
+//   document.getElementById("rocket").style.gridRow = "5";
+// }
+// function rocketspeed11() {
+//   document.getElementById("rocket").style.gridRow = "4";
+// }
+// function rocketspeed12() {
+//   document.getElementById("rocket").style.gridRow = "3";
+// }
+// function rocketspeed13() {
+//   document.getElementById("rocket").style.gridRow = "3";
+// }
+// function rocketspeed14() {
+//   document.getElementById("rocket").style.gridRow = "2";
+// }
+
+// function rocketspeed() {
+//   const myTimeout1e = setTimeout(rocketspeed1, 1000);
+//   const myTimeout2e = setTimeout(rocketspeed2, 2000);
+//   const myTimeout3e = setTimeout(rocketspeed3, 3000);
+//   const myTimeout4e = setTimeout(rocketspeed4, 4000);
+//   const myTimeout5e = setTimeout(rocketspeed5, 5000);
+//   const myTimeout6e = setTimeout(rocketspeed6, 6000);
+//   const myTimeout7e = setTimeout(rocketspeed7, 7000);
+//   const myTimeout8e = setTimeout(rocketspeed8, 8000);
+//   const myTimeout9e = setTimeout(rocketspeed9, 9000);
+//   const myTimeout10e = setTimeout(rocketspeed10, 10000);
+//   const myTimeout11e = setTimeout(rocketspeed11, 11000);
+//   const myTimeout12e = setTimeout(rocketspeed12, 12000);
+//   const myTimeout13e = setTimeout(rocketspeed13, 13000);
+//   const myTimeout14e = setTimeout(rocketspeed14, 14000);
+// }
+// const numbers = [45, 78, 95, 36, 47];
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+// }
--- gamestart.js
+++ gamestart.js
@@ -1,8 +1,8 @@
 function position() {
   document.getElementById("spaceship").style.visibility = "visible";
   document.getElementById("spaceship").style.display = "block";
-  document.getElementById("spaceship").style.gridColumnStart = "4";
-  document.getElementById("spaceship").style.gridColumnEnd = "4";
+  document.getElementById("spaceship").style.gridColumn = "1";
+  document.getElementById("rocket").style.gridColumn = "1";
 }
 function alienposition() {
   document.getElementById("alien1").style.visibility = "visible";
@@ -21,7 +21,7 @@ function alienposition() {
   document.getElementById("alien7").style.display = "block";
   document.getElementById("alien8").style.visibility = "visible";
   document.getElementById("alien8").style.display = "block";
-  document.getElementById("alien1").style.gridColumn = "1/1";
+  document.getElementById("alien1").style.gridArea = "2/1/auto/auto";
   document.getElementById("alien2").style.gridColumn = "2/2";
   document.getElementById("alien3").style.gridColumn = "3/3";
   document.getElementById("alien4").style.gridColumn = "4/4";
@@ -43,69 +43,3 @@ function meteor() {
   document.getElementById("meteor").style.visibility = "visible";
   document.getElementById("meteor").style.display = "block";
 }
-
-// function meteorspeed1() {
-//   document.getElementById(x).style.gridRow = "2";
-// }
-// function meteorspeed2() {
-//   document.getElementById(x).style.gridRow = "3";
-// }
-// function meteorspeed3() {
-//   document.getElementById(x).style.gridRow = "4";
-// }
-// function meteorspeed4() {
-//   document.getElementById(x).style.gridRow = "5";
-// }
-// function meteorspeed5() {
-//   document.getElementById(x).style.gridRow = "6";
-// }
-// function meteorspeed6() {
-//   document.getElementById(x).style.gridRow = "7";
-// }
-// function meteorspeed7() {
-//   document.getElementById(x).style.gridRow = "8";
-// }
-// function meteorspeed8() {
-//   document.getElementById(x).style.gridRow = "9";
-// }
-// function meteorspeed9() {
-//   document.getElementById(x).style.gridRow = "10";
-// }
-// function meteorspeed10() {
-//   document.getElementById(x).style.gridRow = "11";
-// }
-// function meteorspeed11() {
-//   document.getElementById(x).style.gridRow = "12";
-// }
-// function meteorspeed12() {
-//   document.getElementById(x).style.gridRow = "13";
-// }
-// function meteorspeed13() {
-//   document.getElementById(x).style.gridRow = "14";
-// }
-// function meteorspeed14() {
-//   document.getElementById(x).style.gridRow = "15";
-// }
-
-// function meteorspeed() {
-//   const myTimeout1 = setTimeout(meteorspeed1, 1000);
-//   const myTimeout2 = setTimeout(meteorspeed2, 2000);
-//   const myTimeout3 = setTimeout(meteorspeed3, 3000);
-//   const myTimeout4 = setTimeout(meteorspeed4, 4000);
-//   const myTimeout5 = setTimeout(meteorspeed5, 5000);
-//   const myTimeout6 = setTimeout(meteorspeed6, 6000);
-//   const myTimeout7 = setTimeout(meteorspeed7, 7000);
-//   const myTimeout8 = setTimeout(meteorspeed8, 8000);
-//   const myTimeout9 = setTimeout(meteorspeed9, 9000);
-//   const myTimeout10 = setTimeout(meteorspeed10, 10000);
-//   const myTimeout11 = setTimeout(meteorspeed11, 11000);
-//   const myTimeout12 = setTimeout(meteorspeed12, 12000);
-//   const myTimeout13 = setTimeout(meteorspeed13, 13000);
-//   const myTimeout14 = setTimeout(meteorspeed14, 14000);
-// }
-// function meteorspeed2() {
-//   setInterval(function () {
-//     document.getElementById("meteor").style.gridRow = "3";
-//   }, 1000);
-//   clearInterval();
-// }
